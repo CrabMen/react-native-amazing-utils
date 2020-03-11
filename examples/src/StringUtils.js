5396c68c48c56e046cfa5de2e341d59ff77ef2c6
CrabMen <tobecrabman@163.com>
2020-03-11 20:00:29 +0800

    files

---
+++ examples/src/StringUtils.js
@@ -0,0 +1,43 @@
+ export default class StringUtils {
+
+    static isEmpty(s) {
+
+
+    }
+
+    static isNotEmpty(s) {
+
+
+    }
+
+    static isBlank(){
+
+    }
+
+    static capitalize(s){
+
+
+    }
+
+    static uncapitalize(s){
+
+
+    }
+
+   static swapCase(){
+
+    
+   }
+
+    static startsWith(s) {
+
+
+    }
+
+    static endsWith(s) {
+
+        
+    }
+    
+
+}
